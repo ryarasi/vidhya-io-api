b2f79afcca22fe317dd9a02d520e72c43ed74b10
ryarasi <ragav.code@gmail.com>
2021-05-16 14:48:26 +0530

    Updated the database credential details

--- shuddhi/settings.py
+++ shuddhi/settings.py
@@ -83,8 +83,8 @@ WSGI_APPLICATION = 'shuddhi.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'myproject',
-        'USER': 'myprojectuser',
+        'NAME': 'shuddhi',
+        'USER': 'shuddhiadmin',
         'PASSWORD': 'password',
         'HOST': 'localhost',
         'PORT': '',
