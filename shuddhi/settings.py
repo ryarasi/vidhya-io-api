fb1cea1ea18868b07be5e84da1b3629079927b64
ryarasi <ragav.code@gmail.com>
2021-05-17 14:03:18 +0530

    Removed mention of the vidhya app from settings

--- shuddhi/settings.py
+++ shuddhi/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -38,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'graphene_django',
-    'vidhya'
 ]
 
 MIDDLEWARE = [
