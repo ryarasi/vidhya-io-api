682078a14456742f468b38235cd3d64dce2871cc
ryarasi <ragav.code@gmail.com>
2021-05-17 17:09:31 +0530

    Added project level schema and imported the app level schema

---
+++ shuddhi/schema.py
@@ -0,0 +1,17 @@
+import graphene
+import vidhya.schema
+
+
+class Query(vidhya.schema.Query, graphene.ObjectType):
+    # This class will inherit from multiple Queries
+    # as we begin to add more apps to our project
+    pass
+
+
+class Mutation(vidhya.schema.Mutation, graphene.ObjectType):
+    # This class will inherit from multiple Queries
+    # as we begin to add more apps to our project
+    pass
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
