e324dfe9dfcfd8d453f8a093b202a807fe5388c3
ryarasi <ragav.code@gmail.com>
2021-05-19 16:06:43 +0530

    Removed duplicate queries pertaining to graphql_auth

--- shuddhi/schema.py
+++ shuddhi/schema.py
@@ -23,9 +23,6 @@ class AuthMutation(graphene.ObjectType):
 class Query(vidhya.schema.Query, UserQuery, MeQuery, graphene.ObjectType):
     # This class will inherit from multiple Queries
     # as we begin to add more apps to our project
-    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
-    verify_token = graphql_jwt.Verify.Field()
-    refresh_token = graphql_jwt.Refresh.Field()
     pass
 
 
