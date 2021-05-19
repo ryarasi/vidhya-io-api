6bb5d9f54766e8436c64e1bb85e24c399cdbeab4
ryarasi <ragav.code@gmail.com>
2021-05-19 14:00:52 +0530

    Added missing comma

--- shuddhi/settings.py
+++ shuddhi/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'vidhya'
+    'vidhya',
     'graphene_django',
     'graphql_jwt.refresh_token.apps.RefreshTokenConfig'
 ]
