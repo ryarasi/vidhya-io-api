5c3db2cece3c95aacfdd92c80c905a7d9409de48
ryarasi <ragav.code@gmail.com>
2021-05-20 16:36:29 +0530

    Added an index.html page to serve as home page to the API and the supporting code to get it working.

--- shuddhi/settings.py
+++ shuddhi/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -88,7 +89,7 @@ ROOT_URLCONF = 'shuddhi.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -162,6 +163,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    BASE_DIR / "static",
+    '/var/www/static/',
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- vidhya/views.py
+++ vidhya/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+# Create your views here.
+
+
+def index(request):
+    # Render the HTML template index.html
+    context = {}
+    return render(request, 'index.html', context=context)
