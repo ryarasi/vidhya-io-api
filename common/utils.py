5610807aea65a31ba66166bf2d07cae7e816aac6
ryarasi <ragav.code@gmail.com>
2021-05-28 19:22:45 +0530

    Added the last_active updating middleware

--- common/utils.py
+++ common/utils.py
@@ -1,6 +1,10 @@
 from calendar import timegm
 from datetime import datetime
 from .settings import jwt_settings
+from vidhya.models import User
+from django.utils import timezone
+
+# This is used to add the userId as sub to the JWT payload
 
 
 def jwt_payload(user, context=None):
@@ -26,3 +30,19 @@ def jwt_payload(user, context=None):
         payload['iss'] = jwt_settings.JWT_ISSUER
 
     return payload
+
+# This is used to update the lastActive field every time a user makes a request.
+# It helps to track when they were last active.
+
+
+class UpdateLastActivityMiddleware(object):
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        if request.user.is_authenticated:
+            User.objects.filter(pk=request.user.id) \
+                        .update(last_active=timezone.now())
+        response = self.get_response(request)
+
+        return response
