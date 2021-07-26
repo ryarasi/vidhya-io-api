869315d19f16d797c487569bb856e29871940fa6
ryarasi <ragav.code@gmail.com>
2021-07-26 11:49:49 +0530

    Added a priority field to user roles

--- vidhya/models.py
+++ vidhya/models.py
@@ -50,7 +50,7 @@ class User(AbstractUser):
 class UserRole(models.Model):
     name = models.CharField(max_length=50, primary_key=True)
     description = models.CharField(max_length=500,)
-    # priority = models.IntegerField()
+    priority = models.IntegerField()
 
     def default_permissions():
         return {}
