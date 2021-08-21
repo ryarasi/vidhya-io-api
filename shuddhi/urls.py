303d9609326bd85a409ac3a1fb00e50ecabfaad7
ragavyarasi <ragav.code@gmail.com>
2021-08-21 20:32:56 +0530

    added static url in urls

--- shuddhi/urls.py
+++ shuddhi/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('', include('vidhya.urls')),
     path('admin/', admin.site.urls),
     path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
