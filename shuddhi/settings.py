775639b20e7d0459d092dad35ba82a4372235603
ragavyarasi <ragav.code@gmail.com>
2021-08-22 13:51:12 +0530

    Removing file compression

--- shuddhi/settings.py
+++ shuddhi/settings.py
@@ -209,7 +209,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_STORAGE =  'django.contrib.staticfiles.storage.StaticFilesStorage' 
+# STATICFILES_STORAGE =  'django.contrib.staticfiles.storage.StaticFilesStorage' 
 
 # Media files
 MEDIA_URL = '/media/'
