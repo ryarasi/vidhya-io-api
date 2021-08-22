c164b7d46db82c6deb17f4bf4b5fa5911dea897a
ragavyarasi <ragav.code@gmail.com>
2021-08-22 14:01:19 +0530

    Adding compression to the staticfiles

--- shuddhi/settings.py
+++ shuddhi/settings.py
@@ -208,7 +208,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # STATICFILES_STORAGE =  'django.contrib.staticfiles.storage.StaticFilesStorage' 
 
 # Media files
