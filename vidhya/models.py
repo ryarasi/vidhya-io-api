5fe5a2099fa8e658893ba6e6ea3d3f0d7eba889c
ragavyarasi <ragav.code@gmail.com>
2021-09-20 14:54:54 +0530

    Made default rubric an array

--- vidhya/models.py
+++ vidhya/models.py
@@ -322,7 +322,7 @@ class Exercise(models.Model):
     active = models.BooleanField(default=True)
     searchField = models.CharField(max_length=1000, blank=True, null=True)
     def default_rubric():
-        return {}
+        return []
     rubric = JSONField(default=default_rubric)
 
     created_at = models.DateTimeField(auto_now_add=True)
