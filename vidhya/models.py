9f751d940e054bd18e8e18da28fd682144dadcf1
ragavyarasi <ragav.code@gmail.com>
2021-12-04 10:56:14 +0530

    Added created updated fields to completed course

--- vidhya/models.py
+++ vidhya/models.py
@@ -73,14 +73,16 @@ class CompletedChapters(models.Model):
     percentage=models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
+
     def __str__(self):
         return f'{self.chapter.title}' 
 
 class CompletedCourses(models.Model):
     participant = models.ForeignKey(User, on_delete=models.CASCADE)
     course = models.ForeignKey('Course', on_delete=models.CASCADE)
-
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
     def __str__(self):
         return f'{self.course.title}'
 
