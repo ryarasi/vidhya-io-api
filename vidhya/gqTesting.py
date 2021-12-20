cb63041aba034420c50abc5f11f9f7c0a9c78ae5
ragavyarasi <ragav.code@gmail.com>
2021-12-20 19:59:09 +0530

    Added extra testing methods

--- vidhya/gqTesting.py
+++ vidhya/gqTesting.py
@@ -1,6 +1,6 @@
 import graphene
 from graphene_django.types import ObjectType
-from vidhya.models import User
+from vidhya.models import Announcement, User
 from django.conf import settings
 from common.cyTestingData import EXISTING_DATA
 
@@ -17,7 +17,8 @@ class Query(ObjectType):
 
     # Automated Testing related queries
     cy_delete_new_user = graphene.Field(OkResponse)
-
+    cy_create_global_announcement = graphene.Field(OkResponse)
+    cy_delete_global_announcement = graphene.Field(OkResponse)
 
     def resolve_cy_delete_new_user(root, info, **kwargs):
         ok = False
@@ -31,5 +32,34 @@ class Query(ObjectType):
                 ok = True
             except:
                 pass
+        response = OkResponse(ok=ok)
+        return response
+
+    def resolve_cy_create_global_announcement(root, info, **kwargs):
+        ok = False
+        if settings.ENABLED_AUTOMATED_TESTING:
+            input = EXISTING_DATA['newAnnouncement']
+            new_announcement_instance = Announcement(title=input['title'], author_id=input['author'], message=input['message'],
+                                             institution_id=input['institution'], recipients_global=input['recipients_global'])
+            try:
+                new_announcement_instance.save()
+                ok = True
+            except:
+                pass
+        response = OkResponse(ok=ok)
+        return response
+
+    def resolve_cy_delete_global_announcement(root, info, **kwargs):
+        ok = True
+        if settings.ENABLED_AUTOMATED_TESTING:
+            input = EXISTING_DATA['newAnnouncement']
+            all_announcements = Announcement.objects.filter(title=input['title'], author_id=input['author'], message=input['message'],
+                                             institution_id=input['institution'], recipients_global=input['recipients_global'])
+            for announcement in all_announcements:
+                try:
+                    announcement.delete()
+                except:
+                    ok = False
+                    pass
         response = OkResponse(ok=ok)
         return response
