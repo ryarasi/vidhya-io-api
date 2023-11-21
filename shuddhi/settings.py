b3aaf272a4cdba677f81bba3cc62abb1251a5a20
abirami.janarthanam <Abirami.Janarthanam@logicinfo.com>
2023-11-21 13:41:33 +0530

    cleared the comments

--- shuddhi/settings.py
+++ shuddhi/settings.py
@@ -237,8 +237,8 @@ DATABASES = {
 
 DATABASE_URL = os.environ.get('DATABASE_URL')
 db_from_env = dj_database_url.config(default=DATABASE_URL, conn_max_age=500, ssl_require=True)
-# if DEBUG == False:
-DATABASES['default'].update(db_from_env)
+if DEBUG == False:
+    DATABASES['default'].update(db_from_env)
 
 CACHE_HOST='redis'
 CACHE_PORT=6379
