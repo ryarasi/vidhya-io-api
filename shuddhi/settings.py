d17e978ef9e9ad22c6cab160dee76e06ba7e3e1f
Ragav Yarasi <ragav.code@gmail.com>
2024-01-04 14:34:26 +0530

    merge with staging

--- shuddhi/settings.py
+++ shuddhi/settings.py
@@ -237,6 +237,7 @@ DATABASES = {
 
 DATABASE_URL = os.environ.get('DATABASE_URL')
 db_from_env = dj_database_url.config(default=DATABASE_URL, conn_max_age=500, ssl_require=True)
+# if DEBUG == False:
 DATABASES['default'].update(db_from_env)
 
 CACHE_HOST='redis'
